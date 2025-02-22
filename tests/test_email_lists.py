fdbc38e34163f302eef650d9080afd5ef9bb20fe
whoisazamat <whoisazamat@gmail.com>
2025-02-22 22:36:28 +0400

    typo in test data fixed

--- tests/test_email_lists.py
+++ tests/test_email_lists.py
@@ -1,17 +1,17 @@
 def test_email_lists_get_all(email_lists, mock_requestor):
     mock_requestor.send_request.side_effect = [
         {
-            "data": [{"uid": 1, "name": "List 1"}],
+            "data": [{"uuid": 1, "name": "List 1"}],
             "links": {"next": "email-lists?page=2"},
         },
         {
-            "data": [{"uid": 2, "name": "List 2"}],
+            "data": [{"uuid": 2, "name": "List 2"}],
             "links": {"next": None},
         },
     ]
 
     results = list(email_lists.get_all())
-
+    # Rsults should be
     expected_calls = [
         ("GET", "email-lists"),
         ("GET", "email-lists?page=2"),
@@ -19,4 +19,4 @@ def test_email_lists_get_all(email_lists, mock_requestor):
     actual_calls = [(args[0], args[1]) for args, _ in mock_requestor.send_request.call_args_list]
 
     assert actual_calls == expected_calls
-    assert results == [{"uid": 1, "name": "List 1"}, {"uid": 2, "name": "List 2"}]
+    assert results == [{"uuid": 1, "name": "List 1"}, {"uuid": 2, "name": "List 2"}]
