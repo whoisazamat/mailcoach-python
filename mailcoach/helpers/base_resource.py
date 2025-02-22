225e720158633ac033ebc15e44554580b36a9e5f
whoisazamat <whoisazamat@gmail.com>
2025-02-22 22:31:06 +0400

    email lists method get_all actually uses iter_all

--- mailcoach/helpers/base_resource.py
+++ mailcoach/helpers/base_resource.py
@@ -1,6 +1,15 @@
+from collections.abc import Generator
+
 from mailcoach.helpers.requestor import Requestor
 
 
 class BaseResource:
     def __init__(self, requestor: Requestor):
         self.requestor = requestor
+
+    def iter_all(self, endpoint: str) -> Generator[dict, None, None]:
+        while endpoint:
+            response = self.requestor.send_request("GET", endpoint)
+            yield from response.get("data", [])
+
+            endpoint = response.get("links", {}).get("next")
--- mailcoach/resources/email_lists.py
+++ mailcoach/resources/email_lists.py
@@ -1,7 +1,9 @@
+from collections.abc import Generator
+
 from mailcoach.helpers.base_resource import BaseResource
 
 
 class EmailLists(BaseResource):
 
-    def get_all(self) -> dict:
-        return self.requestor.send_request("GET", "email-lists")
+    def get_all(self) -> Generator[dict, None, None]:
+        return self.iter_all("email-lists")
