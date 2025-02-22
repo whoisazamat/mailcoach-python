07360c9a1e22a148f6e053c009fe7333a8759b55
whoisazamat <whoisazamat@gmail.com>
2025-02-22 21:03:54 +0400

    client tests updated

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,3 +5,4 @@ def test_client_initialization():
     client = MailCoachClient(token="test_token_123", url_root="https://api.mailcoach.com")
 
     assert isinstance(client, MailCoachClient)
+    assert isinstance(client, client.email_lists)
