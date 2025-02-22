fc26a6da9ad1e9c3f5e1a38fd1d14cd8144fc6a1
whoisazamat <whoisazamat@gmail.com>
2025-02-22 22:45:07 +0400

    get specific email list method added

--- mailcoach/resources/email_lists.py
+++ mailcoach/resources/email_lists.py
@@ -7,3 +7,10 @@ class EmailLists(BaseResource):
 
     def get_all(self) -> Generator[dict, None, None]:
         return self.iter_all("email-lists")
+
+    def get(self, uuid: str) -> dict:
+        response = self.requestor.send_request(
+            method="GET",
+            url=f"/email-lists/{uuid}",
+        )
+        return response["data"]
