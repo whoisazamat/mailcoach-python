297433c632df99f6940dfc2fb8c6518e1d76fb27
whoisazamat <whoisazamat@gmail.com>
2026-08-24 13:45:20 +0400

    let the transport own its headers

--- mailcoach/client.py
+++ mailcoach/client.py
@@ -1,4 +1,6 @@
-from mailcoach.helpers.requestor import Requestor
+from types import TracebackType
+
+from mailcoach.helpers.requestor import DEFAULT_TIMEOUT, Requestor
 from mailcoach.resources.campaigns import CampaignResource
 from mailcoach.resources.email_lists import EmailListResource
 from mailcoach.resources.segments import SegmentResource
@@ -6,17 +8,27 @@ from mailcoach.resources.tags import TagResource
 
 
 class MailCoachClient:
-    def __init__(self, token: str, url_root: str):
-        requestor = Requestor(
-            url_root=url_root,
-            token=token,
-            request_headers={
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": f"Bearer {token}",
-            },
-        )
-        self.email_lists = EmailListResource(requestor)
-        self.tags = TagResource(requestor)
-        self.segments = SegmentResource(requestor)
-        self.campaigns = CampaignResource(requestor)
+    """Entry point to the Mailcoach API, exposing one attribute per resource."""
+
+    def __init__(self, token: str, url_root: str, timeout: float = DEFAULT_TIMEOUT):
+        self.requestor = Requestor(url_root=url_root, token=token, timeout=timeout)
+
+        self.email_lists = EmailListResource(self.requestor)
+        self.tags = TagResource(self.requestor)
+        self.segments = SegmentResource(self.requestor)
+        self.campaigns = CampaignResource(self.requestor)
+
+    def close(self) -> None:
+        """Release the underlying connection pool."""
+        self.requestor.close()
+
+    def __enter__(self) -> "MailCoachClient":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        self.close()
